90f067a9d8d45a957c58545e2ff6e4cda2226bed
Peter_Liu <heermasterlp@gmail.com>
2018-08-07 11:09:18 +0800

    edit ops

--- utils/ops.py
+++ utils/ops.py
@@ -3,34 +3,42 @@ from __future__ import print_function
 from __future__ import absolute_import
 import tensorflow as tf
 
+KH = 7
+KW = 7
+
 
 def batch_norm(x, is_training, epsilon=1e-5, decay=0.9, scope="batch_norm"):
+    """
+    Batch norm
+    :param x:
+    :param is_training:
+    :param epsilon:
+    :param decay:
+    :param scope:
+    :return:
+    """
     return tf.contrib.layers.batch_norm(x, decay=decay, updates_collections=None, epsilon=epsilon, scale=True,
                                         is_training=is_training, scope=scope)
 
 
-def conv2d(x, output_filters, kh=7, kw=7, sh=2, sw=2, stddev=0.02, scope="conv2d"):
+def conv2d(x, output_filters, kh=KH, kw=KW, sh=2, sw=2, stddev=0.02, scope="conv2d"):
+    """
+    Convolutional layer
+    :param x:
+    :param output_filters:
+    :param kh:
+    :param kw:
+    :param sh:
+    :param sw:
+    :param stddev:
+    :param scope:
+    :return:
+    """
     with tf.variable_scope(scope):
-        # shape = x.get_shape().as_list()
         shape = x.shape
-        # W = tf.get_variable('W', [kh, kw, shape[-1], output_filters],
-        #                     initializer=tf.truncated_normal_initializer(stddev=stddev))
-        # Wconv = tf.nn.conv2d(x, W, strides=[1, sh, sw, 1], padding='SAME')
-        #
-        # biases = tf.get_variable('b', [output_filters], initializer=tf.constant_initializer(0.0))
-        # Wconv_plus_b = tf.reshape(tf.nn.bias_add(Wconv, biases), Wconv.get_shape())
-        #
-        # return Wconv_plus_b
-
         W = tf.get_variable('W', [kh, kw, shape[-1], output_filters],
                             initializer=tf.truncated_normal_initializer(stddev=stddev))
         Wconv = tf.nn.conv2d(x, W, strides=[1, sh, sw, 1], padding='SAME')
-        W1 = tf.get_variable('W1', [kh, kw, output_filters, output_filters],
-                             initializer=tf.truncated_normal_initializer(stddev=stddev))
-        Wconv = tf.nn.conv2d(Wconv, W1, strides=[1, 1, 1, 1], padding='SAME')
-        W2 = tf.get_variable('W2', [kh, kw, output_filters, output_filters],
-                             initializer=tf.truncated_normal_initializer(stddev=stddev))
-        Wconv = tf.nn.conv2d(Wconv, W2, strides=[1, 1, 1, 1], padding='SAME')
 
         biases = tf.get_variable('b', [output_filters], initializer=tf.constant_initializer(0.0))
         Wconv_plus_b = tf.reshape(tf.nn.bias_add(Wconv, biases), Wconv.get_shape())
@@ -38,33 +46,26 @@ def conv2d(x, output_filters, kh=7, kw=7, sh=2, sw=2, stddev=0.02, scope="conv2d
         return Wconv_plus_b
 
 
-def deconv2d(x, output_shape, kh=7, kw=7, sh=2, sw=2, stddev=0.02, scope="deconv2d"):
+def deconv2d(x, output_shape, kh=KH, kw=KW, sh=2, sw=2, stddev=0.02, scope="deconv2d"):
+    """
+    De convolutional layer
+    :param x:
+    :param output_shape:
+    :param kh:
+    :param kw:
+    :param sh:
+    :param sw:
+    :param stddev:
+    :param scope:
+    :return:
+    """
     with tf.variable_scope(scope):
-        # filter : [height, width, output_channels, in_channels]
-        input_shape = x.get_shape().as_list()
-        # W = tf.get_variable('W', [kh, kw, output_shape[-1], input_shape[-1]],
-        #                     initializer=tf.random_normal_initializer(stddev=stddev))
-        #
-        # deconv = tf.nn.conv2d_transpose(x, W, output_shape=output_shape,
-        #                                 strides=[1, sh, sw, 1])
-        #
-        # biases = tf.get_variable('b', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
-        # deconv_plus_b = tf.reshape(tf.nn.bias_add(deconv, biases), deconv.get_shape())
-        #
-        # return deconv_plus_b
-
+        input_shape = x.shape
         W = tf.get_variable('W', [kh, kw, output_shape[-1], input_shape[-1]],
                             initializer=tf.random_normal_initializer(stddev=stddev))
 
-        deconv = tf.nn.conv2d_transpose(x, W, output_shape=output_shape, strides=[1, sh, sw, 1])
-
-        W1 = tf.get_variable('W1', [kh, kw, output_shape[-1], output_shape[-1]],
-                             initializer=tf.random_normal_initializer(stddev=stddev))
-        deconv = tf.nn.conv2d_transpose(deconv, W1, output_shape=output_shape, strides=[1, 1, 1, 1])
-
-        W2 = tf.get_variable('W2', [kh, kw, output_shape[-1], output_shape[-1]],
-                             initializer=tf.random_normal_initializer(stddev=stddev))
-        deconv = tf.nn.conv2d_transpose(deconv, W2, output_shape=output_shape, strides=[1, sh, sw, 1])
+        deconv = tf.nn.conv2d_transpose(x, W, output_shape=output_shape,
+                                        strides=[1, sh, sw, 1])
 
         biases = tf.get_variable('b', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
         deconv_plus_b = tf.reshape(tf.nn.bias_add(deconv, biases), deconv.get_shape())
@@ -73,10 +74,24 @@ def deconv2d(x, output_shape, kh=7, kw=7, sh=2, sw=2, stddev=0.02, scope="deconv
 
 
 def lrelu(x, leak=0.2):
+    """
+    Leaky RELU
+    :param x:
+    :param leak:
+    :return:
+    """
     return tf.maximum(x, leak * x)
 
 
 def fc(x, output_size, stddev=0.02, scope="fc"):
+    """
+    Fully connection layer
+    :param x:
+    :param output_size:
+    :param stddev:
+    :param scope:
+    :return:
+    """
     with tf.variable_scope(scope):
         shape = x.get_shape().as_list()
         W = tf.get_variable("W", [shape[1], output_size], tf.float32,
